698e22a0405a3036034867e6595cbbb66d31b7b3
Pedro Pinho <rihor1000@gmail.com>
2020-07-07 10:38:28 -0300

    Adicionando user_id ao appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,6 +16,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '12312312',
       provider_id: '123',
     });
 
@@ -28,12 +29,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '12312312',
       provider_id: '123',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '12312312',
         provider_id: '123',
       }),
     ).rejects.toBeInstanceOf(AppError);
